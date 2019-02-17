8ff7e131a639c1d6bd48b6035e39d23ad5a80023
Tim Fennell <tim@qualityaspirators.com>
2019-02-17 09:18:22 -0600

    Started lessson 20

---
+++ ACME.Maintenance.Domain.Test/OrderTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACME.Maintenance.Domain.Test
+{
+    [TestClass]
+    public class OrderTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var orderService = new OrderService();
+
+            var contract = new Contract
+            {
+                ContractId = "CONTRACTID",
+                ExpirationDate = DateTime.Now.AddDays(1)
+            };
+
+            var order = orderService.CreateOrder(contract);
+
+            var part = new Part()
+            {
+                PartId = "PARTID",
+                Price = 50.0
+            };
+
+            var orderItem = new OrderItem()
+            {
+                Part = part,
+                Price = part.Price,
+                Quantity = 1,
+                LineTotal = part.Price
+            };
+
+            order.AddOrderItem(orderItem);
+        }
+    }
+}
